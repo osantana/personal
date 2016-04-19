471a09faa7de119e3157e0671eae32beb7edac22
Osvaldo Santana Neto <github@osantana.me>
2016-04-19 00:14:54 -0300

    First version of setup-osx.sh script

---
+++ setup-osx.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Do not open downloaded files after download on Safari
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool NO
+
+# Download PDF instead of open it on Safari
+# defaults write com.apple.Safari WebKitOmitPDFSupport -bool YES
